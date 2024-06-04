97818337f33c5aa548d6e98debc580ca7185a5a7
Quoc Thong Le Gia <56580067+qlegia@users.noreply.github.com>
2024-06-04 14:32:18 +1000

    Update run_time.m

--- M_files/run_time.m
+++ M_files/run_time.m
@@ -1,12 +1,7 @@
 % measure running time
-clear all;
-nn = 10
-for k_exp =1:nn
-  t0 = cputime;
-  %reconstruct_v3_no_noise;
-  %reconstructQRnoise_v2;
-  reconstructSPG_grp1;
-  t1 = cputime;
-  runtime(k_exp)= t1-t0;
-end  
-mean(runtime)
+profile on -detail builtin
+reconstructQRnoise_v2;
+% reconstructSPG_grp1;
+profile report
+% profview
+profile off
